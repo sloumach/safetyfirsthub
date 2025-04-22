99fa135dc71c4171293dee927b42f08f2f8cb46f
sloumach <insloumach@gmail.com>
2025-04-22 12:42:39 +0100

    fix scripts

--- resources/views/adminpanel/courses.blade.php
+++ resources/views/adminpanel/courses.blade.php
@@ -397,5 +397,13 @@
                 loadingState.classList.remove('d-none');
             });
     </script>
+ <!-- Bootstrap core JavaScript-->
+ <script src="adminassets/vendor/jquery/jquery.min.js"></script>
+    <script src="adminassets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
+    <!-- Core plugin JavaScript-->
+    <script src="adminassets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="adminassets/js/sb-admin-2.min.js"></script>
 </html>
