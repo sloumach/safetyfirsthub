28fd591a16b4355c8b996ef924958a3c4bde5d0f
sloumach <insloumach@gmail.com>
2025-04-22 12:48:47 +0100

    fix exam layout

--- resources/views/adminpanel/exams/index.blade.php
+++ resources/views/adminpanel/exams/index.blade.php
@@ -47,13 +47,20 @@
                 <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
-                <div class="container mt-4">
-                    <div class="d-flex justify-content-between align-items-center">
-                        <h2 class="mb-4">Exams Management</h2>
+                <div class="row justify-content-center mt-4">
+
+                        <div class="col-6">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <h2 class="mb-4">Exams Management</h2>
+
+                            </div>
+                        </div>
+
                         <a href="{{ route('admin.exams.create') }}" class="btn btn-primary">
                             <i class="fas fa-plus"></i> Add New Exam
                         </a>
-                    </div>
+
+
 
                     <div class="card shadow-sm">
                         <div class="card-body">
@@ -163,7 +170,7 @@
         </div>
     </div> --}}
 
-  
+
 
 
 
